a9127f2dfa86f69b89cbc32324f90a334d8e594e
Andrew Pickett <picketta@gmail.com>
2019-12-01 00:21:20 -0500

    Finished first puzzle in Python.

--- aoc-2019-python/day1/README.md
+++ aoc-2019-python/day1/README.md
@@ -1,6 +1,13 @@
 https://adventofcode.com/2019/
 
-## --- Day X: Title
+## --- Day 1: The Tyranny of the Rocket Equation ---
 
+What is the sum of the fuel requirements for all of the modules on your spacecraft?
+
+Your puzzle answer was `3382284`.
 
 ## --- Part Two ---
+
+What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the mass of the added fuel? (Calculate the fuel requirements for each module separately, then add them all up at the end.)
+
+Your puzzle answer was `5070541`.
--- aoc-2019-python/day1/main.py
+++ aoc-2019-python/day1/main.py
@@ -1,2 +1,33 @@
+import ir
+import math
+
+
+def calc_mass(val):
+	return math.floor(int(val) / 3) - 2
+
+
+def calc_total(start):
+	if start <= 0:
+		return 0
+	else:
+		return start + calc_total(calc_mass(start))
+
+
+def part_one():
+	total = 0
+	for line in ir.lines:
+		total += calc_mass(line)
+	return total
+
+
+def part_two():
+	total = 0
+	for line in ir.lines:
+		total += calc_total(calc_mass(line))
+	return total
+
+
 if __name__ == '__main__':
-    print('Here we go!')
+	ir.read_lines('./input.txt')
+	print(part_one())
+	print(part_two())
