4795762119044085141b84b9e518700139fd5d74
Andrew Pickett <picketta@gmail.com>
2019-12-03 21:53:10 -0500

    Refactor directory structure/modules. Also made code more Pythonic where possible.

---
+++ 2018/python/day2/main.py
@@ -0,0 +1,14 @@
+data = [int(x.strip()) for x in open("input.txt").readlines()]
+
+
+def part_one():
+	return 0
+
+
+def part_two():
+	return 0
+
+
+if __name__ == '__main__':
+	print(part_one())
+	print(part_two())
