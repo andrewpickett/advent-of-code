41126efd5932b43a6dd40ca64713cbf86743b7af
Andrew Pickett <picketta@gmail.com>
2019-12-05 15:43:06 -0500

    get day6 prepped for python, day2 for julia

---
+++ 2019/julia/src/day2/main.jl
@@ -0,0 +1,44 @@
+data = map(x -> parse(Int64, x), readlines("day2/input.txt"))
+
+function create_data_list(noun, verb)
+   op_data = copy(data)
+   op_data[1] = noun
+   op_data[2] = verb
+   return op_data
+end
+
+function calculate_output(d):
+    for i in 0:4:length(d)
+	 	if d[i] == 99
+			return d[0]
+		end
+		d[d[i+3]] = d[]
+    end
+end
+#
+#
+# def part_one():
+# 	return calculate_output(create_data_list(12, 2))
+#
+#
+# def part_two():
+# 	for noun in range(99):
+# 		for verb in range(99):
+# 			if calculate_output(create_data_list(noun, verb)) == 19690720:
+# 				return noun * 100 + verb
+#
+# def calculate_output(d):
+# 	for i in range(0, len(d), 4):
+# 		op, in1, in2, out = d[i], d[i+1], d[i+2], d[i+3]
+# 		if op == 99:
+# 			return d[0]
+# 		d[out] = d[in1] + d[in2] if op == 1 else d[in1] * d[in2]
+#
+#
+
+function main()
+   println(part_one())
+   println(part_two())
+end
+
+main()
