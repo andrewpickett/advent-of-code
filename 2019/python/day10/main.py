1b4ba8dcea47b9df65845bc948c31319578863a3
Andrew Pickett <picketta@gmail.com>
2019-12-10 07:59:02 -0500

    Start day 10...

--- 2019/python/day10/main.py
+++ 2019/python/day10/main.py
@@ -5,27 +5,28 @@ data = [x.strip()for x in open("input.txt").readlines()]
 
 def part_one():
 	points = []
-	map = []
+	total_objs = 0
 	for i, line in enumerate(data):
-		map.append([])
+		total_objs += line.count('#')
 		for j, pos in enumerate(line):
-			map[i].append(-1 if pos == '.' else 0)
 			if pos == '#':
 				points.append(tuple((j, i)))
 
 	tots = {}
-	for i in range(len(points)):
-		start_point = points[i]
+	for start_point in points:
 		tots[start_point] = set()
-		for j in range(len(points)):
-			end_point = points[j]
+		for end_point in points:
 			if start_point != end_point:
 				# vertical lines first
 				if start_point[0] == end_point[0]:
 					equation = "x={}".format(start_point[0])
+					if end_point[1] < start_point[1]:
+						equation += ' (B)'
 				# horizontal lines next
 				elif start_point[1] == end_point[1]:
 					equation = 'y={}'.format(start_point[1])
+					if end_point[0] < start_point[0]:
+						equation += ' (B)'
 				else:
 					# The rest have slope
 					slope = Fraction(end_point[1] - start_point[1], end_point[0] - start_point[0])
@@ -38,17 +39,17 @@ def part_one():
 						equation += ' (B)'
 				tots[start_point].add(equation)
 
-	pp(tots)
+	# pp(tots)
 	max_val = 0
 	max_pos = (0, 0)
 	for point in tots:
 		if len(tots[point]) > max_val:
 			max_val = len(tots[point])
 			max_pos = point
-	print()
+	# print()
 
-	print("{} at {}".format(max_val, max_pos))
-	return
+	# print("{} at {}".format(max_val, max_pos))
+	return max_val
 
 
 def part_two():
