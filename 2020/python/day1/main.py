03c2cbdfe0d9957cfe8a2da839161ee832f41dd0
Andrew Pickett <picketta@gmail.com>
2020-12-01 00:39:48 -0500

    Solved Day 1 in Python

--- 2020/python/day1/main.py
+++ 2020/python/day1/main.py
@@ -4,13 +4,20 @@ data = [int(x.strip()) for x in open("input.txt").readlines()]
 
 
 def part_one():
-	return 0
+	for i, first in enumerate(data):
+		for second in data[i+1:]:
+			if first + second == 2020:
+				return first * second
 
 
 def part_two():
-	return 0
+	for i, first in enumerate(data):
+		for j, second in enumerate(data[i:]):
+			for third in data[j:]:
+				if first + second + third == 2020:
+					return first * second * third
 
 
 if __name__ == '__main__':
-	run_with_timer(part_one)  #
-	run_with_timer(part_two)  #
+	run_with_timer(part_one)  # 197451 -- took 1 ms
+	run_with_timer(part_two)  # 138233720 -- took 312 ms
