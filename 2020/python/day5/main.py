4a99195e3b737b43c7c4e331e95c39d9b8c96f52
Andrew Pickett <picketta@gmail.com>
2020-12-05 00:36:57 -0500

    Solve Day 5 in Python.

--- 2020/python/day5/main.py
+++ 2020/python/day5/main.py
@@ -1,16 +1,27 @@
 from aoc_utils import run_with_timer
 
-data = [int(x.strip()) for x in open("input.txt").readlines()]
+data = [x.strip() for x in open("input.txt").readlines()]
+
+
+def calc_seat_id(t):
+	return (int(t[:7].replace('F', '0').replace('B', '1'), 2))*8 + int(t[-3:].replace('L', '0').replace('R', '1'), 2)
 
 
 def part_one():
-	return 0
+	return max(calc_seat_id(x) for x in data)
 
 
 def part_two():
-	return 0
+	seat_ids = [-1] * (128*8)
+	for x in data:
+		seat_ids[calc_seat_id(x)] = 1
+
+	for i in range(1, len(seat_ids)-1):
+		if seat_ids[i] == -1 and seat_ids[i-1] == 1 and seat_ids[i+1] == 1:
+			return i
+	return None
 
 
 if __name__ == '__main__':
-	run_with_timer(part_one)  #
-	run_with_timer(part_two)  #
+	run_with_timer(part_one)  # 911 -- took 1 ms
+	run_with_timer(part_two)  # 629 -- took 1 ms
