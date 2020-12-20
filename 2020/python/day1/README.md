ee23c267fcd65f51b175084113f4f30512537ad3
Andrew Pickett <picketta@gmail.com>
2020-12-19 23:20:42 -0500

    Solve Day 19 in Python

--- 2020/python/day1/README.md
+++ 2020/python/day1/README.md
@@ -34,7 +34,8 @@ After saving Christmas five years in a row, you've decided to take a vacation at
 Surely, Christmas will go on without you.
 
 The tropical island has its own currency and is entirely cash-only. The gold coins used there have a little
-picture of a starfish; the locals just call them stars. None of the currency exchanges seem to have heard of them, but somehow, you'll need to find fifty of these coins by the time you arrive so you can pay the deposit on your room.
+picture of a starfish; the locals just call them stars. None of the currency exchanges seem to have heard of them,
+but somehow, you'll need to find fifty of these coins by the time you arrive so you can pay the deposit on your room.
 
 To save your vacation, you need to get all fifty stars by December 25th.
 
--- 2020/python/day19/main.py
+++ 2020/python/day19/main.py
@@ -1,16 +1,41 @@
 from aoc_utils import run_with_timer
 
-data = [int(x.strip()) for x in open("input.txt").readlines()]
+data = [x.strip() for x in open("input.txt").readlines()]
+
+
+def get_input_data():
+	return data[:data.index('')], data[data.index('')+1:]
+
+
+def parse_rule(s):
+	rule_num, rule = s.split(": ")
+	return (int(rule_num), rule) if '"' in rule else (int(rule_num), [[int(y) for y in x.split()] for x in rule.split("|")])
+
+
+def test(rules, s, seq):
+	if s == '' or seq == []:
+		return s == '' and seq == []
+	rule = rules[seq[0]]
+	if '"' in rule:
+		return test(rules, s[1:], seq[1:]) if s[0] in rule else False
+	else:
+		return any(test(rules, s, x+seq[1:]) for x in rule)
 
 
 def part_one():
-	return 0
+	rule_map, tests = get_input_data()
+	rules = dict(parse_rule(x) for x in rule_map)
+	return sum(test(rules, m, [0]) for m in tests)
 
 
 def part_two():
-	return 0
+	rule_map, tests = get_input_data()
+	rule_map[rule_map.index('8: 42')] = '8: 42 | 42 8'
+	rule_map[rule_map.index('11: 42 31')] = '11: 42 31 | 42 11 31'
+	rules = dict(parse_rule(x) for x in rule_map)
+	return sum(test(rules, m, [0]) for m in tests)
 
 
 if __name__ == '__main__':
-	run_with_timer(part_one)  #
-	run_with_timer(part_two)  #
+	run_with_timer(part_one)  # 226 -- took 77 ms
+	run_with_timer(part_two)  # 355 -- took 283 ms
