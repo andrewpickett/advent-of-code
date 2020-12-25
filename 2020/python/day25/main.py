ee016b0db89324e937d0718583ef3a33e9d4f762
Andrew Pickett <picketta@gmail.com>
2020-12-25 00:48:18 -0500

    Solve Day 25 in Python

--- 2020/python/day25/main.py
+++ 2020/python/day25/main.py
@@ -4,7 +4,24 @@ data = [int(x.strip()) for x in open("input.txt").readlines()]
 
 
 def part_one():
-	return 0
+	subject_number = 7
+
+	card_public_key = data[0]
+	door_public_key = data[1]
+
+	door_loop_size = 0
+
+	val = 1
+	while val != door_public_key:
+		val *= subject_number
+		val %= 20201227
+		door_loop_size += 1
+
+	val = 1
+	for i in range(door_loop_size):
+		val *= card_public_key
+		val %= 20201227
+	return val
 
 
 def part_two():
@@ -12,5 +29,5 @@ def part_two():
 
 
 if __name__ == '__main__':
-	run_with_timer(part_one)  #
-	run_with_timer(part_two)  #
+	run_with_timer(part_one)  # 18329280 -- took 4313 ms
+	run_with_timer(part_two)  # 0 -- took 0 ms
