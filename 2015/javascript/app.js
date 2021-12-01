f36290605b14758ecd6ce74b8004b10b06e63d27
Andrew Pickett <picketta@gmail.com>
2021-12-01 11:06:24 -0500

    change port for each javascript node year

--- 2015/javascript/app.js
+++ 2015/javascript/app.js
@@ -17,6 +17,6 @@ app.get('/day/:day/part/:part', function(req, res) {
 	res.json(puzzle.run(parseInt(req.params.part)));
 });
 
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!');
+app.listen(3015, function () {
+	console.log('Example app listening on port 3015!');
 });
