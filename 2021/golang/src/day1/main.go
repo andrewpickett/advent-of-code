aa0ef9ede1d2dc973e63965fb6311dedd82062e8
Andrew Pickett <picketta@gmail.com>
2021-12-01 10:47:43 -0500

    Complete day 1 for 2021 in golang

--- 2021/golang/src/day1/main.go
+++ 2021/golang/src/day1/main.go
@@ -1,24 +1,44 @@
 package main
 
 import (
+	"strconv"
 	"utils"
 )
 
 var data = getData(utils.GetLines("input.txt"))
 
-func getData(lines []string) []string {
-	return lines
+func getData(lines []string) []int {
+	var retVal []int
+	for _, e := range lines {
+		i, _ := strconv.Atoi(e)
+		retVal = append(retVal, i)
+	}
+	return retVal
 }
 
 func partOne() string {
-	return ""
+	count := 0
+	for i := 1; i < len(data); i++ {
+		if data[i] > data[i-1] {
+			count += 1
+		}
+	}
+
+	return strconv.Itoa(count)
 }
 
 func partTwo() string {
-	return ""
+	count := 0
+	for i := 3; i < len(data); i++ {
+		if data[i]+data[i-1]+data[i-2] > data[i-1]+data[i-2]+data[i-3] {
+			count += 1
+		}
+	}
+
+	return strconv.Itoa(count)
 }
 
 func main() {
-	utils.RunWithTimer(partOne) // main.partOne --  -- took 0 ms
-	utils.RunWithTimer(partTwo) // main.partTwo --  -- took 0 ms
+	utils.RunWithTimer(partOne) // 1766 -- took 0 ms
+	utils.RunWithTimer(partTwo) // 1797 -- took 0 ms
 }
