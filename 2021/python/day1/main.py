85141eeb88251e4dced6584e04b59a48cfcfb10e
Andrew Pickett <picketta@gmail.com>
2021-12-01 10:39:54 -0500

    Complete day 1 for 2021!! WOOHOO!

--- 2021/python/day1/main.py
+++ 2021/python/day1/main.py
@@ -4,13 +4,13 @@ data = [int(x.strip()) for x in open("input.txt").readlines()]
 
 
 def part_one():
-	return
+	return sum(1 for i in range(1, len(data)) if data[i] > data[i-1])
 
 
 def part_two():
-	return
+	return sum(1 for i in range(3, len(data)) if data[i] + data[i-1] + data[i-2] > data[i-1] + data[i-2] + data[i-3])
 
 
 if __name__ == '__main__':
-	run_with_timer(part_one)  #
-	run_with_timer(part_two)  #
+	run_with_timer(part_one)  # 1766 -- took 0 ms
+	run_with_timer(part_two)  # 1797 -- took 0 ms
