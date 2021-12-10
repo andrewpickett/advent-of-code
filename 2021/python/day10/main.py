a9a693c52536d57739adc1c288405977c5c02f8e
Andrew Pickett <picketta@gmail.com>
2021-12-10 15:07:52 -0500

    Solve day 10 in python for 2021.

--- 2021/python/day10/main.py
+++ 2021/python/day10/main.py
@@ -2,48 +2,40 @@ from aoc_utils import run_with_timer
 
 data = [list(x.strip()) for x in open("input.txt").readlines()]
 
-start_delims = '([{<'
-end_delims = ')]}>'
-points = [3, 57, 1197, 25137]
-closing_points = [1, 2, 3, 4]
+START_DELIMS = '([{<'
+END_DELIMS = ')]}>'
+PTS = {"syntax": [3, 57, 1197, 25137], "autocorrect": [1, 2, 3, 4]}
+
+
+def analyze_code(code):
+	st = []
+	syntax_pts = 0
+	for y in code:
+		if y in START_DELIMS:
+			st.append(y)
+		else:
+			if START_DELIMS[END_DELIMS.find(y)] == st[-1]:
+				st.pop()
+			else:
+				syntax_pts += PTS["syntax"][END_DELIMS.find(y)]
+				break
+	return syntax_pts, st
 
 
 def part_one():
-	total = 0
-	for x in data:
-		st = []
-		for y in x:
-			if y in start_delims:
-				st.append(y)
-			else:
-				if start_delims[end_delims.find(y)] == st[-1]:
-					st.pop()
-				else:
-					total += points[end_delims.find(y)]
-					break
-	return total
+	return sum(analyze_code(x)[0] for x in data)
 
 
 def part_two():
 	scores = []
 	for x in data:
-		corrupt = False
-		st = []
-		for y in x:
-			if y in start_delims:
-				st.append(y)
-			else:
-				if start_delims[end_delims.find(y)] == st[-1]:
-					st.pop()
-				else:
-					corrupt = True
-					break
-		if not corrupt and len(st) > 0:
+		syntax_pts, st = analyze_code(x)
+		if syntax_pts <= 0 and len(st) > 0:
 			total = 0
 			while len(st) > 0:
 				p = st.pop()
 				total *= 5
-				total += closing_points[start_delims.find(p)]
+				total += PTS["autocorrect"][START_DELIMS.find(p)]
 			scores.append(total)
 	return sorted(scores)[len(scores) // 2]
 
