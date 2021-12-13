5c2db44e9230eb3e95fd8ae7b3bdbeacc2e67370
andrewpickett <picketta@gmail.com>
2021-12-13 10:39:52 -0500

    Complete day 12 for 2021 in Python.

--- 2021/python/day12/main.py
+++ 2021/python/day12/main.py
@@ -1,7 +1,7 @@
 from aoc_utils import run_with_timer
 import copy
 
-data = [x.strip().split("-") for x in open("sample.txt").readlines()]
+data = [x.strip().split("-") for x in open("input.txt").readlines()]
 
 
 def build_map():
@@ -18,62 +18,42 @@ def build_map():
 	return m
 
 
-def count_ways_to_node(m, current_path, full_paths):
-	last_node = current_path[-1]
-	if last_node == 'end':
-		full_paths.add('-'.join(current_path))
-		return
-	if last_node.islower():
-		for x in m:
-			if last_node in m[x]:
-				m[x].remove(last_node)
-	if len(m[last_node]) > 0:
-		for x in m[last_node]:
-			next_path = current_path.copy()
-			next_path.append(x)
-			count_ways_to_node(copy.deepcopy(m), next_path, full_paths)
-
-
-def count_ways_to_node2(m, current_path, full_paths):
-	# print(m, current_path, full_paths)
-	last_node = current_path[-1]
-	if last_node == 'end':
-		full_paths.add('-'.join(current_path))
-		return 1
-	if last_node.islower():
-		if sum(1 for x in current_path if x == last_node) == 2 and last_node != 'start':
-			print("SECOND VISIT OF", last_node)
-			for x in m:
-				m[x] = list(filter(lambda y: y.isupper() or (y.islower() and y not in current_path), m[x]))
-		# for x in m:
-		# 	if node in m[x]:
-		# 		m[x].remove(node)
-	if len(m[last_node]) > 0:
-		for x in m[last_node]:
-			next_path = current_path.copy()
-			next_path.append(x)
-			count_ways_to_node2(copy.deepcopy(m), next_path, full_paths)
+def count_ways_to_node(m, current_path, full_paths, second_visit):
+    last_node = current_path[-1]
+    if last_node == 'end':
+        full_paths.add('-'.join(current_path))
+        return
+    if last_node.islower():
+        if not second_visit:
+            if sum(1 for x in current_path if x == last_node) == 2 and last_node != 'start':
+                second_visit = True
+                for x in m:
+                    m[x] = list(filter(lambda y: y.isupper() or (y.islower() and y not in current_path), m[x]))
+        else:
+            for x in m:
+                if last_node in m[x]:
+                    m[x].remove(last_node)
+    if len(m[last_node]) > 0:
+        for x in m[last_node]:
+            next_path = current_path.copy()
+            next_path.append(x)
+            count_ways_to_node(copy.deepcopy(m), next_path, full_paths, second_visit)
 
 
 def part_one():
-	m = build_map()
-	full_paths = set()
-	count_ways_to_node(copy.deepcopy(m), ['start'], full_paths)
-	for x in sorted(full_paths):
-		print(x)
-	return len(full_paths)
+    m = build_map()
+    full_paths = set()
+    count_ways_to_node(copy.deepcopy(m), ['start'], full_paths, True)
+    return len(full_paths)
 
 
 def part_two():
-	m = build_map()
-	full_paths = set()
-	# print(m)
-	count_ways_to_node2(copy.deepcopy(m), ['start'], full_paths)
-	for x in sorted(full_paths):
-		print(x)
-	return len(full_paths)
+    m = build_map()
+    full_paths = set()
+    count_ways_to_node(copy.deepcopy(m), ['start'], full_paths, False)
+    return len(full_paths)
 
 
 if __name__ == '__main__':
-	run_with_timer(part_one)  #
-	run_with_timer(part_two)  #
+	run_with_timer(part_one)  # 5252 -- took 433 ms
+	run_with_timer(part_two)  # 147784 -- took 12627 ms
