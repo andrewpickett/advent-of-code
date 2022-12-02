b034073d165b78c329065762dbfc416a984cfcf0
Andrew Pickett <picketta@gmail.com>
2022-12-02 15:17:26 -0500

    Day 9 of 2016.

--- 2016/python/day9/main.py
+++ 2016/python/day9/main.py
@@ -3,41 +3,25 @@ from aoc_utils import run_with_timer
 data = open("input.txt").readline().strip()
 
 
-def part_one():
-	return decompress(data, False)
-	# new_str = ""
-	# i = 0
-	# while i < len(data):
-	# 	if data[i] == "(":
-	# 		close_idx = data[i:].find(")")
-	# 		parts = data[i+1:i+close_idx].split("x")
-	# 		new_str += data[i+close_idx+1:i+close_idx+1+int(parts[0])] * int(parts[1])
-	# 		i += 1+close_idx+int(parts[0])
-	# 	else:
-	# 		new_str += data[i]
-	# 		i += 1
-	# return new_str
-
-
-def decompress(s, recurse):
-	if s.find("(") < 0:
+def decompress(s, recurse, depth):
+	sidx = s.find("(")
+	eidx = s.find(")")
+	if sidx < 0 or (not recurse and depth > 0):
 		return len(s)
 
-	prefix = s[0:s.find("(")]
-	parts = s[s.find("(")+1:s.find(")")].split("x")
+	prefix = s[0:sidx]
+	parts = s[sidx + 1:eidx].split("x")
+	next = s[eidx + 1:eidx + 1 + int(parts[0])]
+	suffix = s[eidx+1+int(parts[0]):]
+	return len(prefix) + int(parts[1]) * decompress(next, recurse, depth+1) + decompress(suffix, recurse, depth)
 
-	next = s[s.find(")")+1:s.find(")")+1+int(parts[0])]
 
-	suffix = s[s.find(")")+1+int(parts[0]):]
-
-	if recurse:
-		return len(prefix) + int(parts[1]) * decompress(next, True) + decompress(suffix, True)
-	else:
-		return len(prefix + int(parts[1])) * len(next) + decompress(suffix, False)
+def part_one():
+	return decompress(data, False, 0)
 
 
 def part_two():
-	return decompress(data, True)
+	return decompress(data, True, 0)
 
 
 if __name__ == '__main__':
