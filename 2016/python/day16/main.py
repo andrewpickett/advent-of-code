58d48977eee2280b43fe161292ed362b195dabe9
Andrew Pickett <picketta@gmail.com>
2022-12-07 11:36:45 -0500

    2016 Day 16

--- 2016/python/day16/main.py
+++ 2016/python/day16/main.py
@@ -1,14 +1,31 @@
 from aoc_utils import run_with_timer
 
-data = [int(x.strip()) for x in open("input.txt").readlines()]
+data = open("input.txt").readline().strip()
+
+
+def generate_data(a, disc_size):
+	d = a
+	while len(d) < disc_size:
+		d = d + "0" + d[::-1].replace("0", "2").replace("1", "0").replace("2", "1")
+	return d[:disc_size]
+
+
+def generate_checksum(s):
+	checksum = s
+	while len(checksum) % 2 == 0:
+		new_checksum = ""
+		for i in range(0, len(checksum), 2):
+			new_checksum += "1" if checksum[i:i+2] in ["00", "11"] else "0"
+		checksum = new_checksum
+	return checksum
 
 
 def part_one():
-	return
+	return generate_checksum(generate_data(data, 272))
 
 
 def part_two():
-	return
+	return generate_checksum(generate_data(data, 35651584))
 
 
 if __name__ == '__main__':
