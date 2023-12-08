273d08f792db154457e023316e9c51496c08574e
Andrew Pickett <picketta@gmail.com>
2023-12-08 09:25:57 -0500

    Redo 2015.

--- 2015/python/day16/main.py
+++ 2015/python/day16/main.py
@@ -1,6 +1,10 @@
-from aoc_utils import run_with_timer
+from utils.timers import run_with_timer
 
-data = [x.strip() for x in open('input.txt').readlines()]
+
+def get_data(filename):
+	return [x.strip() for x in open(filename).readlines()]
+
+
 ticker = {
 	"children": 3,
 	"cats": 7,
@@ -15,24 +19,17 @@ ticker = {
 }
 
 
-def get_sues_list():
-	return [{parts[i*2+2][:-1]: int(parts[i*2+3].replace(',', '')) for i in range(len(parts)//2 - 1)} for parts in [x.split() for x in data]]
+def get_sues_list(d):
+	return [{parts[i*2+2][:-1]: int(parts[i*2+3].replace(',', '')) for i in range(len(parts)//2 - 1)} for parts in [x.split() for x in d]]
 
 
-def part_one():
-	sues = get_sues_list()
-	for sue in get_sues_list():
-		match_found = True
-		for prop in ticker.keys():
-			if prop in sue.keys() and ticker[prop] != sue[prop]:
-				match_found = False
-		if match_found:
-			return sues.index(sue) + 1
-	return
+def part_one(d):
+	sues = get_sues_list(d)
+	return [sues.index(sue)+1 for sue in sues if sum(1 if sue[x] == ticker[x] else 0 for x in sue.keys() & ticker.keys()) == len(sue)][0]
 
 
-def part_two():
-	sues = get_sues_list()
+def part_two(d):
+	sues = get_sues_list(d)
 	for sue in sues:
 		match_found = True
 		for prop in ticker.keys():
@@ -51,5 +48,6 @@ def part_two():
 
 
 if __name__ == '__main__':
-	run_with_timer(part_one)  # 40 -- took 2 ms
-	run_with_timer(part_two)  # 241 -- took 1 ms
+	data = get_data("input.txt")
+	run_with_timer(part_one, data)  # 40 -- took 2 ms
+	run_with_timer(part_two, data)  # 241 -- took 1 ms
