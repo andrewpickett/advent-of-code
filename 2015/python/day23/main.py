824fc0dabc07eaaeb38300a684409f7502c5cae4
Andrew Pickett <picketta@gmail.com>
2023-12-15 12:55:03 -0500

    Redo 2015

--- 2015/python/day23/main.py
+++ 2015/python/day23/main.py
@@ -1,47 +1,50 @@
-from aoc_utils import run_with_timer
-
-data = [x.strip().split(" ") for x in open('input.txt').readlines()]
+from utils.timers import run_with_timer
 
 
-def run_code(registers):
+def get_data(filename):
+	return [x.strip().split(" ") for x in open(filename).readlines()]
+
+
+def run_code(d, registers):
 	inst_idx = 0
-	while inst_idx < len(data):
-		inst = data[inst_idx][0]
+	while inst_idx < len(d):
+		inst = d[inst_idx][0]
 		if inst == 'hlf':
-			registers[data[inst_idx][1]] //= 2
+			registers[d[inst_idx][1]] //= 2
 			inst_idx += 1
 		elif inst == 'tpl':
-			registers[data[inst_idx][1]] *= 3
+			registers[d[inst_idx][1]] *= 3
 			inst_idx += 1
 		elif inst == 'inc':
-			registers[data[inst_idx][1]] += 1
+			registers[d[inst_idx][1]] += 1
 			inst_idx += 1
 		elif inst == 'jmp':
-			inst_idx += int(data[inst_idx][1])
+			inst_idx += int(d[inst_idx][1])
 		elif inst == 'jie':
-			if registers[data[inst_idx][1][:-1]] % 2 == 0:
-				inst_idx += int(data[inst_idx][2])
+			if registers[d[inst_idx][1][:-1]] % 2 == 0:
+				inst_idx += int(d[inst_idx][2])
 			else:
 				inst_idx += 1
 		elif inst == 'jio':
-			if registers[data[inst_idx][1][:-1]] == 1:
-				inst_idx += int(data[inst_idx][2])
+			if registers[d[inst_idx][1][:-1]] == 1:
+				inst_idx += int(d[inst_idx][2])
 			else:
 				inst_idx += 1
 
 
-def part_one():
+def part_one(d):
 	registers = {"a": 0, "b": 0}
-	run_code(registers)
+	run_code(d, registers)
 	return registers["b"]
 
 
-def part_two():
+def part_two(d):
 	registers = {"a": 1, "b": 0}
-	run_code(registers)
+	run_code(d, registers)
 	return registers["b"]
 
 
 if __name__ == '__main__':
-	run_with_timer(part_one)  #
-	run_with_timer(part_two)  #
+	data = get_data("input.txt")
+	run_with_timer(part_one, data)
+	run_with_timer(part_two, data)
