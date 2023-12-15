da7df10e8ad00b42a0db69efa111df979e091183
Andrew Pickett <picketta@gmail.com>
2023-12-15 14:52:26 -0500

    Redo 2015

--- 2015/python/day25/main.py
+++ 2015/python/day25/main.py
@@ -1,6 +1,8 @@
-from aoc_utils import run_with_timer
+from utils.timers import run_with_timer
 
-data = open('input.txt').readline().strip().split(" ")
+
+def get_data(filename):
+	return open(filename).readline().strip().split(" ")
 
 
 def get_fill_order(row, col):
@@ -8,18 +10,19 @@ def get_fill_order(row, col):
 	return (tmp * (tmp+1) // 2) + col
 
 
-def part_one():
-	fill_order = get_fill_order(int(data[16][:-1]), int(data[18][:-1]))
+def part_one(d):
+	fill_order = get_fill_order(int(d[16][:-1]), int(d[18][:-1]))
 	val = 20151125
 	for i in range(2, fill_order+1):
 		val = (val * 252533) % 33554393
 	return val
 
 
-def part_two():
-	return
+def part_two(d):
+	return True
 
 
 if __name__ == '__main__':
-	run_with_timer(part_one)  #
-	run_with_timer(part_two)  #
+	data = get_data("input.txt")
+	run_with_timer(part_one, data)
+	run_with_timer(part_two, data)
