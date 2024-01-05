9b2fae7057069a8e8bbdc4f66afe79899381bdb7
Andrew Pickett <picketta@gmail.com>
2024-01-05 09:00:44 -0500

    Redo 2016.

--- 2016/python/day2/main.py
+++ 2016/python/day2/main.py
@@ -1,6 +1,9 @@
-from aoc_utils import run_with_timer
+from utils.timers import run_with_timer, get_data_with_timer
 
-data = [x.strip() for x in open("input.txt").readlines()]
+
+def get_data(filename):
+	return [x.strip() for x in open(filename).readlines()]
+
 
 keypad1 = [
 	['1', '2', '3'],
@@ -8,48 +11,48 @@ keypad1 = [
 	['7', '8', '9']
 ]
 keypad2 = [
-	['0', '0', '1', '0', '0'],
-	['0', '2', '3', '4', '0'],
+	[' ', ' ', '1', ' ', ' '],
+	[' ', '2', '3', '4', ' '],
 	['5', '6', '7', '8', '9'],
-	['0', 'A', 'B', 'C', '0'],
-	['0', '0', 'D', '0', '0']
+	[' ', 'A', 'B', 'C', ' '],
+	[' ', ' ', 'D', ' ', ' ']
 ]
 
 
 def get_code_digit(keypad, line, start_point):
-	curr_row = start_point[0]
-	curr_col = start_point[1]
+	r = start_point[0]
+	c = start_point[1]
 	for y in line:
 		match y:
 			case 'U':
-				curr_row = curr_row - 1 if curr_row > 0 and keypad[curr_row-1][curr_col] != '0' else curr_row
+				r = r - 1 if r > 0 and keypad[r-1][c] != ' ' else r
 			case 'D':
-				curr_row = curr_row + 1 if curr_row < len(keypad)-1 and keypad[curr_row+1][curr_col] != '0' else curr_row
+				r = r + 1 if r < len(keypad)-1 and keypad[r+1][c] != ' ' else r
 			case 'L':
-				curr_col = curr_col - 1 if curr_col > 0 and keypad[curr_row][curr_col-1] != '0' else curr_col
+				c = c - 1 if c > 0 and keypad[r][c-1] != ' ' else c
 			case 'R':
-				curr_col = curr_col + 1 if curr_col < len(keypad)-1 and keypad[curr_row][curr_col+1] != '0' else curr_col
-	return keypad[curr_row][curr_col], (curr_row, curr_col)
+				c = c + 1 if c < len(keypad)-1 and keypad[r][c+1] != ' ' else c
+	return keypad[r][c], (r, c)
 
 
-def part_one():
-	curr_pos = (1, 1)
+def get_code(d, start_pos, keypad):
+	curr_pos = start_pos
 	code = ""
-	for x in data:
-		digit, curr_pos = get_code_digit(keypad1, x, curr_pos)
+	for x in d:
+		digit, curr_pos = get_code_digit(keypad, x, curr_pos)
 		code += digit
 	return code
 
 
-def part_two():
-	curr_pos = (2, 0)
-	code = ""
-	for x in data:
-		digit, curr_pos = get_code_digit(keypad2, x, curr_pos)
-		code += digit
-	return code
+def part_one(d):
+	return get_code(d, (1, 1), keypad1)
+
+
+def part_two(d):
+	return get_code(d, (2, 0), keypad2)
 
 
 if __name__ == '__main__':
-	run_with_timer(part_one)  # 84452 -- took 0 ms
-	run_with_timer(part_two)  # D65C3 -- took 0 ms
+	data = get_data_with_timer(get_data, "input.txt")
+	run_with_timer(part_one, data)
+	run_with_timer(part_two, data)
