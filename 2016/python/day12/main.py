0653b35847e2a478f7e5b0574cb16c0f495eb015
Andrew Pickett <picketta@gmail.com>
2024-01-09 22:03:31 -0500

    Redo 2016.

--- 2016/python/day12/main.py
+++ 2016/python/day12/main.py
@@ -1,34 +1,39 @@
-from aoc_utils import run_with_timer
-
-data = [x.strip().split(" ") for x in open("input.txt").readlines()]
+from utils.timers import run_with_timer, get_data_with_timer
 
 
-def run_input(registers, output_val):
+def get_data(filename):
+	return [x.strip().split(" ") for x in open(filename).readlines()]
+
+
+def run_input(d, registers, output_val):
 	p = 0
-	while p < len(data):
-		match data[p][0]:
-			case "cpy":
-				registers[data[p][2]] = int(data[p][1]) if data[p][1].isnumeric() else registers[data[p][1]]
-				p += 1
-			case "inc":
-				registers[data[p][1]] += 1
-				p += 1
-			case "dec":
-				registers[data[p][1]] -= 1
-				p += 1
-			case "jnz":
-				p += 1 if (data[p][1].isnumeric() and int(data[p][1]) == 0) or (not data[p][1].isnumeric() and registers[data[p][1]] == 0) else int(data[p][2])
+	while p < len(d):
+		inst = d[p][0]
+		v = int(d[p][1]) if d[p][1].isnumeric() else registers[d[p][1]]
+		if inst == "cpy":
+			registers[d[p][2]] = v
+			p += 1
+		elif inst == "jnz":
+			p += 1 if v == 0 else int(d[p][2])
+		else:
+			registers[d[p][1]] += 1 if inst == "inc" else -1
+			p += 1
 	return registers[output_val]
 
 
-def part_one():
-	return run_input({"a": 0, "b": 0, "c": 0, "d": 0}, "a")
+def part_one(d):
+	return run_input(d, {"a": 0, "b": 0, "c": 0, "d": 0}, "a")
 
 
-def part_two():
-	return run_input({"a": 0, "b": 0, "c": 1, "d": 0}, "a")
+def part_two(d):
+	return run_input(d, {"a": 0, "b": 0, "c": 1, "d": 0}, "a")
+
+
+def main(f="input.txt"):
+	data = get_data_with_timer(get_data, f)
+	run_with_timer(part_one, data)
+	run_with_timer(part_two, data)
 
 
 if __name__ == '__main__':
-	run_with_timer(part_one)  # 318003 -- took 663 ms
-	run_with_timer(part_two)  # 9227657 -- took 19928 ms
+	main()
