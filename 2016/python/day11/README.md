c2c501b4c8a5e75cd924c68b349d0f7bd5dca855
Andrew Pickett <picketta@gmail.com>
2024-02-07 13:02:37 -0500

    Redo 2016.

--- 2016/python/day11/README.md
+++ 2016/python/day11/README.md
@@ -2,14 +2,23 @@
 ### Part 1
 > _In your situation, what is the minimum number of steps required to bring all of the objects to the fourth floor?_
 
+...I solved it by hand. I think I get how to do it (using some sort of Dijkstra or A* to traverse ahead and start figuring
+out the "shortest" solve...obviously stopping the process if it's a state I've seen before as that would cause infinite loops).
+I started writing things up, and kept fumbling up on it, so I sat down and decided to see
+if I could just solve it by hand. Sure enough, in about 10 minutes, I had what I thought was the right answer. I plugged it
+in, and it was right.
+
+So, I do need to go back to this one and actually code it up. It reminds me a lot of a Towers of Hanoi type of problem...
+Pretty simple to actually solve by hand, but writing the code is not as easy as you'd think...
 
 ### Part 2
-> __
+> _What is the minimum number of steps required to bring all of the objects, including these four new ones, to the fourth floor?_
 
+Yeah, same thing. It took a little longer, but solving by hand worked just fine here...
 
 # Results
 
 |              | Exec. Time (ms) |
 |--------------|----------------:|
-| **Part One** |                 |
-| **Part Two** |                 |
+| **Part One** |   N/A (for now) |
+| **Part Two** |   N/A (for now) |
--- 2016/python/day11/main.py
+++ 2016/python/day11/main.py
@@ -4,27 +4,45 @@ from utils.timers import run_with_timer, get_data_with_timer
 def get_data(filename):
 	return [x.strip() for x in open(filename).readlines()]
 
+# def parse_input(d):
+# 	elevator_floor = 0
+# 	floors = [["PG", "PM"], ["CG", "UG", "RG", "PG"], ["CM", "UM", "RM", "PM"], []]
+# 	for i, x in enumerate(d):
+# 		if "nothing relevant" not in x:
+# 			parts = x.split(" ")
+# 			for j, y in enumerate(parts):
+# 				if y == "a":
+# 					part = (parts[j+1][0:3] + " " + parts[j+2][0:3]).upper()
+# 					if part.endswith(".") or part.endswith(","):
+# 						part = part[:-1]
+# 					floors[i].append(part)
+# 	return floors
 
-def parse_input(d):
-	floors = [[], [], [], []]
-	for i, x in enumerate(d):
-		if "nothing relevant" not in x:
-			parts = x.split(" ")
-			for j, y in enumerate(parts):
-				if y == "a":
-					part = (parts[j+1][0:3] + " " + parts[j+2][0:3]).upper()
-					if part.endswith(".") or part.endswith(","):
-						part = part[:-1]
-					floors[i].append(part)
-	return floors
+def find_valid_two_up(current_floor, target):
+	pass
 
 
 def part_one(d):
-	floors = parse_input(d)
-	print(floors)
+	# floors = parse_input(d)
+	# print(floors)
+	# floors = [{"g": ["p"], "m": ["p"]}, {"g": ["c", "u", "r", "l"], "m": []}, {"g": [], "m": ["c", "u", "r", "l"]}, {"g": [], "m": []}]
+	floors = [{"g": [], "m": ["h", "l"]}, {"g": ["h"], "m": []}, {"g": [], "m": ["l"]}, {"g": [], "m": []}]
 	curr_floor = 0
 	move_count = 0
 	while len(floors[0]) != 0 or len(floors[1]) != 0 or len(floors[2]) != 0:
+		# Elevator can only move with 1 or 2 items (either type)
+		# can't move mixed M and Gs of different types together
+		# M and G on the same floor without matching the M and G is not allowed
+		# if I ever "undo" a move I just did, then stop that branch (if recursing)??
+
+		# assumptions:
+			# best to move 2 up if possible
+			# never move 2 down
+
+		# Find all valid 2 up moves...then find all valid 1 up moves...then find all valid 1 down moves
+
+		# What is a "move" -- it is a target floor, and 1 or 2 elements
+
 		move_count += 1
 	return move_count
 
