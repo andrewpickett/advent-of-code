668aaea09360bc496b954e464552ccf6be11899a
Andrew Pickett <picketta@gmail.com>
2024-10-21 17:35:00 -0400

    Redo 2017.

--- 2017/python/day25/README.md
+++ 2017/python/day25/README.md
@@ -1,18 +1,44 @@
 # Approach
 ### Data format
 
+I read the input file into an object which contains the different states and their `0` and `1` configurations. This
+allowed me to just jump around and load the next state and its associated behavior really simply. The resulting
+map looked something like:
+```
+{
+	"A": {
+		"0": {
+			"w": #write value,
+			"m": #move value (-1 or 1),
+			"s": #state to jump to value
+		},
+		"1": {
+			"w": #write value,
+			"m": #move value (-1 or 1),
+			"s": #state to jump to value
+		}
+	}
+	...
+}
+```
+
+I then stored that in a map along with the initial state and the number of times to run.
 
 ### Part 1
-> __
+> _What is the diagnostic checksum it produces once it's working again?_
 
+Nothing fancy. Use the map I created above and just actually run through it the number of times it said. I knew it
+would take a little while, but 12 million is doable, so I gave it a shot. Took about 10 seconds, but finished...so...
+Since it was the last puzzle, I was happy and just accepted it.
 
 ### Part 2
 > __
 
+Click the button, and we're done. Merry Christmas!!!!
 
 # Results
 
 |              | Exec. Time (ms) |
 |--------------|----------------:|
-| **Part One** |                 |
+| **Part One** |            9131 |
 | **Part Two** |                 |
--- 2017/python/day25/main.py
+++ 2017/python/day25/main.py
@@ -2,22 +2,48 @@ from utils.timers import run_with_timer, get_data_with_timer
 
 
 def get_data(filename):
-    return open(filename).readline().strip()
+	lines = [x.strip() for x in open(filename).readlines()]
+	data = {"steps": int(lines[1].split()[-2]), "states": {}, "start": lines[0].split()[-1][:-1]}
+	for x in range(0, len(lines[3:]), 10):
+		data["states"][lines[3:][x][-2]] = {
+			"0": {
+				"w": int(lines[3:][x+2].split()[-1][:-1]),
+				"m": 1 if lines[3:][x+3].find("right") > 0 else -1,
+				"s": lines[3:][x+4].split()[-1][:-1]
+			},
+			"1": {
+				"w": int(lines[3:][x+6].split()[-1][:-1]),
+				"m": 1 if lines[3:][x+7].find("right") > 0 else -1,
+				"s": lines[3:][x+8].split()[-1][:-1]
+			}
+		}
+	return data
 
 
 def part_one(d):
-    return
+	curr_pos = 0
+	vals = {}
+	curr_state = d["states"][d["start"]]
+	for x in range(d["steps"]):
+		str_pos = str(curr_pos)
+		state_val = "0" if str_pos not in vals or vals[str_pos] == 0 else "1"
+		if str_pos not in vals:
+			vals[str_pos] = "0"
+		vals[str_pos] = curr_state[state_val]["w"]
+		curr_pos += curr_state[state_val]["m"]
+		curr_state = d["states"][curr_state[state_val]["s"]]
+	return sum(1 for x in vals if vals[x] == 1)
 
 
 def part_two(d):
-    return
+	return
 
 
 def main(f="input.txt"):
-    data = get_data_with_timer(get_data, f)
-    run_with_timer(part_one, data)
-    run_with_timer(part_two, data)
+	data = get_data_with_timer(get_data, f)
+	run_with_timer(part_one, data)
+	run_with_timer(part_two, data)
 
 
 if __name__ == '__main__':
-    main()
+	main()
