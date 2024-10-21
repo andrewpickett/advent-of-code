9be061bc1b0d7a87d66fa894180b0958635414a0
Andrew Pickett <picketta@gmail.com>
2024-10-21 15:51:51 -0400

    Redo 2017.

--- 2017/python/day24/README.md
+++ 2017/python/day24/README.md
@@ -1,18 +1,34 @@
 # Approach
 ### Data format
 
+Alright, the basic idea for the way to think through this problem is as a graph (more explanation as to why in the Part 1
+section below). But, since we don't actually need the connections, I decided to just keep a list of all of the points
+as well as a list of all of the possible starting points (those that have a `0` in them).
 
 ### Part 1
-> __
+> _What is the strength of the strongest bridge you can make with the components you have available?_
 
+First step is that we need to run some algorithm from each and every starting node (nodes that have `0` in them).
+As such, we know we're going to loop over those possible starts. I then decided to simply do a depth-first traversal
+of the "graph" created by conceptually linking the nodes together without any repeats. Whenever we get to a point
+where there are no possible remaining nodes "connected", we simply add that path to the list of possible paths.
+
+In the end, we will end up with a list of all possible paths. We don't care about fragments of a path, since we are only
+looking for the "longest" (e.g. `0/1` doesn't matter once we have `0/1 --> 10/1`...because there's no way `0/1` could
+ever be longer by itself). So, we only need to store fully developed paths.
+
+Once the list of all possible paths is complete, just find the one that has the highest weight. Done. It takes a little
+while to run, but it's not too horrible.
 
 ### Part 2
-> __
+> _What is the strength of the longest bridge you can make?_
 
+Well, since I have all of the paths, just loop over them saving the longest ones. Then at the end just get the weights
+of these longest ones. Done and done.
 
 # Results
 
 |              | Exec. Time (ms) |
 |--------------|----------------:|
-| **Part One** |                 |
-| **Part Two** |                 |
+| **Part One** |            4088 |
+| **Part Two** |            3076 |
--- 2017/python/day24/main.py
+++ 2017/python/day24/main.py
@@ -2,22 +2,72 @@ from utils.timers import run_with_timer, get_data_with_timer
 
 
 def get_data(filename):
-    return open(filename).readline().strip()
+	all_data = {"starts": []}
+	all_pts = [tuple(map(int, x.strip().split("/"))) for x in open(filename).readlines()]
+	for x in all_pts:
+		if 0 in x:
+			all_data["starts"].append(x)
+	all_data["points"] = all_pts
+	return all_data
 
 
 def part_one(d):
-    return
+	return max(calc_weight(x) for x in get_all_paths(d))
 
 
 def part_two(d):
-    return
+	all_paths = get_all_paths(d)
+	longest = []
+	longest_len = 0
+	for x in all_paths:
+		if len(x) > longest_len:
+			longest.clear()
+			longest_len = len(x)
+		if len(x) >= longest_len:
+			longest.append(x)
+	return max(calc_weight(x) for x in longest)
+
+
+def get_all_paths(d):
+	all_paths = []
+	for x in d["starts"]:
+		remaining_points = d["points"].copy()
+		remaining_points.remove(x)
+		find_path([], x, 0, remaining_points, all_paths)
+	return all_paths
+
+
+def find_path(curr_path, curr_point, last_in, remaining_points, all_paths):
+	out_coord = curr_point[0] if curr_point[1] == last_in else curr_point[1]
+	next_pts = get_next_available_points(out_coord, remaining_points)
+	curr_path.append(curr_point)
+	if not next_pts:
+		all_paths.append(curr_path)
+	else:
+		for x in next_pts:
+			r = remaining_points.copy()
+			r.remove(x)
+			find_path(curr_path.copy(), x, out_coord, r, all_paths)
+	return curr_path
+
+
+def get_next_available_points(out_coord, remaining_points):
+	available = []
+	for x in remaining_points:
+		if out_coord in x:
+			available.append(x)
+	return available
+
+
+def calc_weight(path):
+	return sum(x[0] + x[1] for x in path)
 
 
 def main(f="input.txt"):
-    data = get_data_with_timer(get_data, f)
-    run_with_timer(part_one, data)
-    run_with_timer(part_two, data)
+	data = get_data_with_timer(get_data, f)
+	run_with_timer(part_one, data)
+	run_with_timer(part_two, data)
 
 
 if __name__ == '__main__':
-    main()
+	main()
