62a2052e8a1ee3d81ab6a2b43003a0b859a89416
Andrew Pickett <picketta@gmail.com>
2024-10-29 12:20:13 -0400

    Redo 2018.

--- 2018/python/day13/README.md
+++ 2018/python/day13/README.md
@@ -1,17 +1,43 @@
 # Approach
 ### Data format
 
+Read the input into a 2-d array. Then go through and find every cart in the array (`>`, `^`, `<`, `v`) and replace the
+value in the array with the actual track value "under" that cart. Each of the "carts" is represented by
+```
+(y-coord, x-coord, direction, intersection_counter)
+```
+
+Because it's a tuple ordered like this, I can just run a `.sort()` on the list of them and it will order them by row and
+then column so I can process them in the correct way.
+
+I then also store an array for tracking any collisions.
 
 ### Part 1
-> __
+> _To help prevent crashes, you'd like to know the location of the first crash._
+
+In concept, this is pretty simple: for each "cart" in my list, just increment them in the direction they're going.
+We then just need to check if any two carts are on the same point. So really, just implement the following:
+1) movement logic
+   1) intersection logic
+   2) turning logic
+2) collision detection logic
+
+Then just run through "move, detect" cycles until there has been a collision detected.
+
+It worked great at first, but I realized pretty quickly that I was only doing the collision detection at the END
+of the entire turn. Instead I needed to check after every single movement. I then ended up going back and forth
+multiple times trying to write it in a way that made the most sense and wasn't overly ugly.
+
+It took a lot longer than I wanted, but in the end it's pretty good.
 
 ### Part 2
-> __
+> _What is the location of the last cart at the end of the first tick where it is the only cart left?_
 
+Run through the same code, but this time only stop when the number of carts left in the list is 0 or 1.
 
 # Results
 
 |              | Exec. Time (ms) |
 |--------------|----------------:|
-| **Part One** |                 |
-| **Part Two** |                 |
+| **Part One** |               7 |
+| **Part Two** |             207 |
--- 2018/python/day13/main.py
+++ 2018/python/day13/main.py
@@ -1,22 +1,87 @@
 from utils.timers import run_with_timer, get_data_with_timer
+from utils.utils import DIRS
+from copy import deepcopy
+
+TURNS = {
+	"/": {"v": "<", ">": "^", "<": "v", "^": ">"},
+	"\\": {"v": ">", ">": "v", "<": "^", "^": "<"},
+	"+": {
+		"^": ["<", "^", ">"],
+		">": ["^", ">", "v"],
+		"v": [">", "v", "<"],
+		"<": ["v", "<", "^"]}
+}
 
 
 def get_data(filename):
-	return [x.strip() for x in open(filename).readlines()]
+	ret_val = {"tracks": [[y for y in x] for x in open(filename).readlines()], "carts": [], "collisions": []}
+	for y, row in enumerate(ret_val["tracks"]):
+		for x, col in enumerate(row):
+			if col in [">", "<", "^", "v"]:
+				ret_val["carts"].append((y, x, col, 0))
+				if col in ["v", "^"]:
+					ret_val["tracks"][y][x] = "|"
+				elif col in [">", "<"]:
+					ret_val["tracks"][y][x] = "-"
+	return ret_val
 
 
 def part_one(d):
-	return
+	while not move_carts(d):
+		d["carts"].sort()
+	return "{},{}".format(d["collisions"][0][0], d["collisions"][0][1])
 
 
 def part_two(d):
-	return
+	while len(d["carts"]) > 1:
+		d["carts"].sort()
+		move_carts(d)
+	return "{},{}".format(d["collisions"][0][0], d["collisions"][0][1]) if len(d["carts"]) == 0 else "{},{}".format(d["carts"][0][1], d["carts"][0][0])
+
+
+def move_carts(d):
+	ci = 0
+	while ci < len(d["carts"]):
+		cart = d["carts"].pop(ci)
+		next_y = cart[0] + DIRS[cart[2]][0]
+		next_x = cart[1] + DIRS[cart[2]][1]
+		next_counter = cart[3]
+		if d["tracks"][next_y][next_x] == "+":
+			next_counter = (cart[3] + 1) % 3
+
+		new_cart = (next_y, next_x, turn(cart[2], d["tracks"][next_y][next_x], cart[3]), next_counter)
+		i = check_collisions(new_cart, d["carts"])
+		if i is not None:
+			d["carts"].pop(i)
+			d["collisions"].append((next_x, next_y))
+			if i < ci:
+				ci -= 1
+		else:
+			d["carts"].insert(0, new_cart)
+			ci += 1
+	return len(d["collisions"]) > 0
+
+
+def check_collisions(new_cart, other_carts):
+	for i, x in enumerate(other_carts):
+		if new_cart[0] == x[0] and new_cart[1] == x[1]:
+			return i
+	return None
+
+
+def turn(curr_dir, track, counter):
+	if track in ["/", "\\"]:
+		return TURNS[track][curr_dir]
+	elif track == "+":
+		return TURNS[track][curr_dir][counter]
+	else:
+		return curr_dir
 
 
 def main(f="input.txt"):
 	data = get_data_with_timer(get_data, f)
-	run_with_timer(part_one, data)
-	run_with_timer(part_two, data)
+	run_with_timer(part_one, deepcopy(data))
+	run_with_timer(part_two, deepcopy(data))
 
 
 if __name__ == '__main__':
