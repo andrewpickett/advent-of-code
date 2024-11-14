b9a6a8788ea54f40d24e0121a32e1abd2c9c4443
Andrew Pickett <picketta@gmail.com>
2024-11-14 13:22:54 -0500

    Redo 2018.

--- 2018/python/day17/README.md
+++ 2018/python/day17/README.md
@@ -1,17 +1,73 @@
 # Approach
 ### Data format
 
+I started by creating a map with all of the information that would be needed, which included: the minimum `y` value
+in the input, the starting point, and then the `Grid` that I create from the input. So I used my `Grid` again, and
+then iterated over the input to actually set the values to be accurate.
+
+Once I had that, I solved part 1, but then realized part 2 would just use the exact same output from part 1...so
+I instead did the "solve" in the process of getting the data and stored the actual FINAL output to the data object
+that I can just pass to my part 1 and part 2 code -- making them really trivial. So I'll describe the actual solve
+in the `Part 1` section below...but in the end, I actually do the work in the setup, so I can use it across both
+parts.
 
 ### Part 1
-> __
+> _How many tiles can the water reach within the range of y values in your scan?_
+
+So this was fun for a while...and it wasn't too difficult at first. Using my `Grid` class, I was able to instantiate the
+input exactly as described. I then started work on the algorithm to actually have the water flow. I basically broke it down
+to this:
+1. From the start point, fall until I hit a "solid" (`#`).
+2. From there, check if the row I'm on will overflow either left or right
+   1. If it will NOT overflow, simply fill that row from the left `#` to the right `#` with `~` and then move up a row and repeat this step.
+   2. If it will overflow, fill that row with `|` from the place it overflows to the opposite wall. Return the position of the overflow
+3. Now, repeat the last 2 steps using the last overflow position and it SHOULD just flow down and down and down...
+
+Right?
+
+Well, the first use case that broke this logic was if it overflows on BOTH sides. Well now I had to make sure I ran the
+algorithm for ALL places where water is falling. So, that means maintaining a stack of overflow points. Now I just need to
+keep iterating through the algorithm until there are no more points that are "falling".
+
+The "end" cases were hitting the bottom of the grid or hitting an already full "bucket". Alright! now it should work!!
+
+...right??
+
+Nope...the big use case that I was missing was if there was a "box" inside of a "bucket". So this use case:
+```
+.....|....
+.#...|..#.
+.#..###.#.
+.#..#.#.#.
+.#..###.#.
+.#......#.
+.#......#.
+.########.
+```
+When this happened, my overflow logic just got completely destroyed. So I revamped it all, and changed how I detect when it
+overflows and just made sure that it would work for this case as well.
+
+Once that use case worked, it all flowed down so nicely. Store the resulting grid (with all of the flows) into my data.
+
+Now just count the number of `~` and `|` and we should be good!
+
+...right??????
+
+Nope...I misread that you shouldn't include any water that's "above" the highest point in your input. So I had to add
+one final piece to count how much water was above the highest defined point and subtract that from my final answer.
+
+FINALLY!!
 
 ### Part 2
-> __
+> _How many water tiles are left after the water spring stops producing water and all remaining water not at rest has drained?_
 
+Since I already have the full data built out, it's just counting the number of `~` characters...don't need to worry about
+the `|` because they will all just "go away" when the water stops...and don't have to worry about anything "above" the highest
+point, since no water would be there anyways...Very simple.
 
 # Results
 
 |              | Exec. Time (ms) |
 |--------------|----------------:|
-| **Part One** |                 |
-| **Part Two** |                 |
+| **Part One** |             118 |
+| **Part Two** |             120 |
--- 2018/python/day17/main.py
+++ 2018/python/day17/main.py
@@ -1,22 +1,116 @@
 from utils.timers import run_with_timer, get_data_with_timer
+from utils.grid import Grid
 
 
 def get_data(filename):
-	return [x.strip() for x in open(filename).readlines()]
+	lines = [x.strip() for x in open(filename).readlines()]
+	ranges = []
+	ret_val = {}
+	for line in lines:
+		parts = line.split(", ")
+		a = range(int(parts[0][2:]), int(parts[0][2:]) + 1)
+		b = range(int(parts[1][2:parts[1].find("..")]), int(parts[1][parts[1].find("..")+2:]) + 1)
+		ranges.append((b, a) if parts[0].startswith("x") else (a, b))
+
+	ret_val["miny"] = min(y.start for y, _ in ranges)
+	ret_val["xoff"] = min(x.start for _, x in ranges) - 1
+	grid = Grid(height=max(y.stop for y, _ in ranges), width=max(x.stop for _, x in ranges)-ret_val["xoff"]+1, default_value=".")
+	grid.set_neighbors_for_all()
+	for y, x in ranges:
+		for row in y:
+			for col in x:
+				grid.get_point(row, col-ret_val["xoff"]).set_value("#")
+	ret_val["start"] = (0, 500-ret_val["xoff"])
+	ret_val["grid"] = waterfall(grid, ret_val["start"])
+
+	below_min = 0
+	for y in range(ret_val["miny"]):
+		below_row = ''.join([x.get_value() for x in ret_val["grid"].get_row(y)])
+		below_min += below_row.count("|") + below_row.count("~")
+	ret_val["below"] = below_min
+	return ret_val
 
 
 def part_one(d):
-	return
+	result = d["grid"].output()
+	return result.count("~") + result.count("|") - d["below"]
 
 
 def part_two(d):
-	return
+	return d["grid"].output().count("~")
+
+
+def waterfall(grid, start):
+	grid.get_point(start[0], start[1]).set_value("+")
+	curr_pos = start
+	fall_points = [curr_pos]
+	fallen = set(curr_pos)
+	while fall_points:
+		curr_pos = fall_points.pop(0)
+		curr_pos = fall(grid, (curr_pos[0]+1, curr_pos[1]))
+		if curr_pos:
+			left_overflow, right_overflow, boundaries = check_overflow(grid, curr_pos)
+			while not left_overflow and not right_overflow:
+				fill(boundaries, "~")
+				curr_pos = (curr_pos[0]-1, curr_pos[1])
+				left_overflow, right_overflow, boundaries = check_overflow(grid, curr_pos)
+			fill(boundaries, "|")
+			if left_overflow and boundaries[0].get_coord() not in fallen:
+				fall_points.append(boundaries[0].get_coord())
+				fallen.add(boundaries[0].get_coord())
+			if right_overflow and boundaries[1].get_coord() not in fallen:
+				fall_points.append(boundaries[1].get_coord())
+				fallen.add(boundaries[1].get_coord())
+	return grid
+
+
+def fall(g, p):
+	next_point = g.get_point(p[0], p[1])
+	while next_point.get_value() == ".":
+		next_point.set_value("|")
+		p = (p[0] + 1, p[1])
+		if p[0] >= g.get_height() or g.get_point(p[0], p[1]).get_value() == "|":
+			return False
+		next_point = g.get_point(p[0], p[1])
+	return p[0] - 1, p[1]
+
+
+def fill(boundaries, fill_char):
+	curr_point = boundaries[0]
+	while curr_point != boundaries[1] and curr_point.get_value() != "#":
+		curr_point.set_value(fill_char)
+		curr_point = curr_point.get_east_neighbor()
+	curr_point.set_value(fill_char)
+
+
+def check_overflow(g, p):
+	boundaries = []
+	lover = check_overflow_dir(g, p, boundaries, True)
+	rover = check_overflow_dir(g, p, boundaries, False)
+	return lover, rover, boundaries
+
+
+def check_overflow_dir(g, p, boundaries, left):
+	next_point = g.get_point(coords=p)
+	overflow = False
+	neighbor = next_point.get_west_neighbor() if left else next_point.get_east_neighbor()
+	sneigh = next_point.get_south_neighbor()
+	while neighbor and sneigh and sneigh.get_value() != "." and neighbor.get_value() != "#":
+		next_point = neighbor
+		neighbor = next_point.get_west_neighbor() if left else next_point.get_east_neighbor()
+		sneigh = next_point.get_south_neighbor()
+		if sneigh.get_value() == ".":
+			overflow = True
+		elif neighbor and neighbor.get_value() == "#":
+			overflow = False
+	boundaries.append(next_point)
+	return overflow
 
 
 def main(f="input.txt"):
 	data = get_data_with_timer(get_data, f)
-	run_with_timer(part_one, data)
-	run_with_timer(part_two, data)
+	run_with_timer(part_one, data)  # 36790
+	run_with_timer(part_two, data)  # 30765
 
 
 if __name__ == '__main__':
