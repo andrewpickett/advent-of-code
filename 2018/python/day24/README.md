5419ecb2307ecbc550467d9d241130c66a5b9e8c
Andrew Pickett <picketta@gmail.com>
2024-11-15 14:14:02 -0500

    Redo 2018.

--- 2018/python/day24/README.md
+++ 2018/python/day24/README.md
@@ -1,17 +1,45 @@
 # Approach
 ### Data format
 
+I originally tried just making a dictionary holding all of the properties, but I was getting into issues with pointers
+and finally decided to just create objects.
+
+So basically I just read the input and store all of the groups into custom `Group` classes and then store a list of those
+into an `Army` class. Now I can just manipulate the objects in place.
 
 ### Part 1
-> __
+> _As it stands now, how many units would the winning army have?_
+
+Break it up into the different phases:
+1. Target Selection
+	* Just loop over all the enemies and decide which target each attacker will attack and then set it on the object.
+2. Attack
+   * Now for each of the targets assigned, just lower the number of units.
+3. Reset
+   * Clear the targets and just move on.
+
+The only "gotcha" here was to make sure to not account for any groups that had 0 units. I could have removed them from
+the list completely, but figured, it didn't hurt to just keep them in there...I just had to add a few checks to make sure
+they were never targeted.
+
+End the fighting once either army has no more units in all of the groups.
 
 ### Part 2
-> __
+> _How many units does the immune system have left after getting the smallest boost it needs to win?_
 
+Just brute force it. Start at a `boost` of 1 and try the fight. Check if immunity army wins. If not, then increse the boost
+by 1 and try again. Just keep doing this until the immunity army is the one that wins.
+
+The big catch here is that there are times when either army can't kill any more units. Since we discard any "remainder"
+attack power, it means they will just continually fight forever not killing each other. So to solve for this, I just put
+in a check for how many units are in the fight at the beginning and then how many are at the end. If ever it's the same
+number, you know you're deadlocked and we can just quit then and there.
+
+So...doing that, it worked in about 15 seconds. Not bad...and good enough for me.
 
 # Results
 
 |              | Exec. Time (ms) |
 |--------------|----------------:|
-| **Part One** |                 |
-| **Part Two** |                 |
+| **Part One** |              84 |
+| **Part Two** |           14345 |
--- 2018/python/day24/main.py
+++ 2018/python/day24/main.py
@@ -1,16 +1,172 @@
 from utils.timers import run_with_timer, get_data_with_timer
+from copy import deepcopy
+
+
+class Army:
+	def __init__(self, name):
+		self.name = name
+		self.groups = []
+
+	def add_group(self, group):
+		self.groups.append(group)
+
+	def has_fighters(self):
+		return sum(1 for x in self.groups if x.units > 0)
+
+	def remove_targets(self):
+		for x in self.groups:
+			x.target = None
+
+	def __str__(self):
+		s = self.name + ":\n"
+		for x in self.groups:
+			if x.units > 0:
+				s += str(x) + "\n"
+		return s
+
+	def __repr__(self):
+		return self.__str__()
+
+
+class Group:
+	def __init__(self, army, label, line):
+		parts = line.split()
+		self.army = army
+		self.label = label
+		self.units = int(parts[0])
+		self.hp = int(parts[4])
+		self.weakness = extract_properties(parts, "weak")
+		self.immunity = extract_properties(parts, "immune")
+		self.attack = int(parts[parts.index("attack")+3])
+		self.type = parts[parts.index("attack")+4]
+		self.initiative = int(parts[-1])
+		self.target = None
+		self.effective_power = self.units * self.attack
+		self.dmg = 0
+
+	def destroy_units(self, units):
+		self.units -= units
+		self.effective_power = self.units * self.attack
+		self.dmg = calc_damage(self, self.target)
+
+	def __str__(self):
+		return "Group {} contains {} units".format(self.label, self.units)
+
+	def __repr__(self):
+		return self.__str__()
+
+	def __eq__(self, other):
+		return self.label == other.label and self.army == other.army
+
+	def __lt__(self, other):
+		if self.effective_power == other.effective_power:
+			return self.initiative > other.initiative
+		else:
+			return self.effective_power > other.effective_power
 
 
 def get_data(filename):
-	return [x.strip() for x in open(filename).readlines()]
+	immune_system = Army("Immune System")
+	infection = Army("Infection")
+
+	lines = [x.strip() for x in open(filename).readlines()]
+	curr_army = immune_system
+	label = 1
+	for line in lines[1:]:
+		if line != "":
+			if line == "Infection:":
+				curr_army = infection
+				label = 1
+			else:
+				curr_army.add_group(Group(curr_army.name, label, line))
+				label += 1
+	return [immune_system, infection]
 
 
 def part_one(d):
-	return
+	armies = deepcopy(d)
+	while armies[0].has_fighters() and armies[1].has_fighters():
+		fight(armies)
+	return sum(x.units for x in armies[0].groups) + sum(x.units for x in armies[1].groups)
 
 
 def part_two(d):
-	return
+	boost = 0
+	while True:
+		boost += 1
+		armies = deepcopy(d)
+		for x in armies[0].groups:
+			x.attack += boost
+			x.effective_power = x.units * x.attack
+		last_total_unit_count = 0
+		total_count = sum(x.units for x in armies[0].groups) + sum(x.units for x in armies[1].groups)
+		while armies[0].has_fighters() and armies[1].has_fighters() and total_count != last_total_unit_count:
+			total_count = sum(x.units for x in armies[0].groups) + sum(x.units for x in armies[1].groups)
+			fight(armies)
+			last_total_unit_count = sum(x.units for x in armies[0].groups) + sum(x.units for x in armies[1].groups)
+		if armies[0].has_fighters() and not armies[1].has_fighters():
+			break
+	return sum(x.units for x in armies[0].groups)
+
+
+def fight(d):
+	for army in d:
+		army.groups.sort()
+	select_targets(d[1], d[0])
+	select_targets(d[0], d[1])
+	attack_targets(d[0], d[1])
+	d[0].remove_targets()
+	d[1].remove_targets()
+
+
+def select_targets(attackers, defenders):
+	selected_targets = []
+	for i in attackers.groups:
+		target = None
+		max_dmg = 0
+		for j in [x for x in defenders.groups if x not in selected_targets and x.units > 0]:
+			dmg = calc_damage(i, j)
+			if dmg > max_dmg:
+				max_dmg = dmg
+				target = j
+			elif dmg == max_dmg and dmg != 0:
+				if j.effective_power > target.effective_power:
+					target = j
+				elif j.effective_power == target.effective_power:
+					target = j if j.initiative > target.initiative else target
+		if target:
+			i.dmg = max_dmg
+			i.target = target
+			selected_targets.append(target)
+
+
+def calc_damage(attacker, defender):
+	dmg = attacker.effective_power
+	if defender:
+		if attacker.type in defender.weakness:
+			dmg *= 2
+		elif attacker.type in defender.immunity:
+			dmg = 0
+	return dmg
+
+
+def attack_targets(immunities, infections):
+	all_attackers = list(reversed(sorted(immunities.groups + infections.groups, key=lambda item: item.initiative)))
+	for x in all_attackers:
+		if x.target:
+			units_killed = min(x.dmg // x.target.hp, x.target.units)
+			x.target.destroy_units(units_killed)
+
+
+def extract_properties(parts, prop):
+	props = []
+	if "(" + prop in parts or prop in parts:
+		idx = parts.index("(" + prop) if "(" + prop in parts else parts.index(prop)
+		for x in parts[idx+2:]:
+			props.append(x[:-1])
+			if x.endswith(";") or x.endswith(")"):
+				break
+	return props
 
 
 def main(f="input.txt"):
