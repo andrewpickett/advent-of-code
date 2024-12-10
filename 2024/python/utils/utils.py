c6c1cd1177ab058b34a9f2962b3d5e12ee082211
Andrew Pickett <picketta@gmail.com>
2024-12-10 14:05:35 -0500

    Day 10 was nice and easy.

--- 2024/python/utils/utils.py
+++ 2024/python/utils/utils.py
@@ -31,6 +31,49 @@ DIR_LOOKUP = {
 DIR_COORDS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 
+def read_input_as_2d_int_array(file, pad_size=0, pad_val=-1):
+	lines = [x.strip() for x in open(file).readlines()]
+	if pad_size > 0:
+		ret_val = []
+		ns_pad = [[pad_val] * (len(lines[0])+(2*pad_size)) for _ in range(pad_size)]
+		ret_val.extend(ns_pad.copy())
+		ret_val.extend([[pad_val] * pad_size + list(map(int, [y for y in x])) + [pad_val] * pad_size for x in lines])
+		ret_val.extend(ns_pad.copy())
+		return ret_val
+	else:
+		return [list(map(int, [y for y in x])) for x in lines]
+
+
+def read_input_as_2d_str_array(file, pad_size=0, pad_val="."):
+	lines = [x.strip() for x in open(file).readlines()]
+	if pad_size > 0:
+		ret_val = []
+		ns_pad = [[pad_val] * (len(lines[0])+(2*pad_size)) for _ in range(pad_size)]
+		ret_val.extend(list(ns_pad))
+		ret_val.extend([list(x) for x in [pad_val * pad_size + x + pad_val * pad_size for x in lines]])
+		ret_val.extend(list(ns_pad))
+		return ret_val
+	else:
+		return [list(x) for x in lines]
+
+
+def tuple_add(array_of_tuples, scalar_tuple):
+	if array_of_tuples is None:
+		return None
+	if len(array_of_tuples[0]) != len(scalar_tuple):
+		raise ValueError("Tuples must have same length")
+	return [tuple(a+b for a, b in zip(t, scalar_tuple)) for t in array_of_tuples]
+
+
+def tuple_multiply(array_of_tuples, scalar_tuple):
+	if array_of_tuples is None:
+		return None
+	if len(array_of_tuples[0]) != len(scalar_tuple):
+		raise ValueError("Tuples must have same length")
+	return [tuple(a*b for a, b in zip(t, scalar_tuple)) for t in array_of_tuples]
+
+
+
 def turn_right(curr_dir):
 	if curr_dir == DIRS["U"]:
 		return DIRS["R"]
