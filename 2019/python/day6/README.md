5be0228b768ae098d83404c7148e605b94c941dd
Andrew Pickett <picketta@gmail.com>
2024-12-13 13:16:09 -0500

    Redo 2019.

--- 2019/python/day6/README.md
+++ 2019/python/day6/README.md
@@ -1,14 +1,26 @@
 # Approach
 ### Data format
 
+I decided to build a tree as part of the input. Basically create a map of the location as the key, and a `TreeNode` as
+the value. If there is already a `TreeNode` with that key, then append the location as a child.
 
 ### Part 1
-> __
+> _What is the total number of direct and indirect orbits in your map data?_
 
+Now that I have a full tree, it's pretty simple to just loop over every single node, get the tree from it, and count
+how long that specific branch is. I do that for every single node/point. This will by nature count all of them (even the
+subtrees) because every node has a full chain back to the root to count.
+
+Add them all up, and we're done.
 
 ### Part 2
-> __
+> _What is the minimum number of orbital transfers required to move from the object YOU are orbiting to the object SAN is orbiting?_
 
+At its core, all this part of the puzzle is is to find the distance between the two nodes and their first common
+ancestor. So I just start with the `YOU` node and start traversing up the tree. At each point, check if that node is
+in the tree for `SAN`. Once we find one, we know that is the first common ancestor...which means we just need to count
+how far up the tree it is from both of the nodes and add it together. Since I have the ancestry stored as a list in both
+nodes, I can just get the indexes of that ancestor in both chains and add them together.
 
 # Results
 
--- 2019/python/day6/main.py
+++ 2019/python/day6/main.py
@@ -1,6 +1,9 @@
-data = [tuple(x.strip().split(')')) for x in open("input.txt").readlines()]
+from utils.timers import run_with_timer, get_data_with_timer
 
-planets = {}
+
+def get_data(filename):
+	data = [tuple(x.strip().split(')')) for x in open(filename).readlines()]
+	return build_orbit_tree(data)
 
 
 class TreeNode:
@@ -18,12 +21,12 @@ class TreeNode:
 			ancestor = ancestor.parent_node
 
 
-def build_orbit_tree():
+def build_orbit_tree(d):
 	orbitals = {}
-	for d in data:
-		orbitals[d[0]] = TreeNode() if d[0] not in orbitals else orbitals[d[0]]
-		orbitals[d[1]] = TreeNode() if d[1] not in orbitals else orbitals[d[1]]
-		orbitals[d[0]].add_child(orbitals[d[1]])
+	for x in d:
+		orbitals[x[0]] = TreeNode() if x[0] not in orbitals else orbitals[x[0]]
+		orbitals[x[1]] = TreeNode() if x[1] not in orbitals else orbitals[x[1]]
+		orbitals[x[0]].add_child(orbitals[x[1]])
 
 	for orbital in orbitals.values():
 		orbital.build_orbit_chain()
@@ -31,13 +34,13 @@ def build_orbit_tree():
 	return orbitals
 
 
-def part_one():
-	return sum(len(planets[x].orbit_chain) for x in planets)
+def part_one(d):
+	return sum(len(d[x].orbit_chain) for x in d)
 
 
-def part_two():
-	my_chain = planets['YOU'].orbit_chain
-	san_chain = planets['SAN'].orbit_chain
+def part_two(d):
+	my_chain = d['YOU'].orbit_chain
+	san_chain = d['SAN'].orbit_chain
 
 	common_ancestor = None
 	for _, elem in enumerate(my_chain):
@@ -47,7 +50,11 @@ def part_two():
 	return my_chain.index(common_ancestor) + san_chain.index(common_ancestor)
 
 
+def main(f="input.txt"):
+	data = get_data_with_timer(get_data, f)
+	run_with_timer(part_one, data)
+	run_with_timer(part_two, data)
+
+
 if __name__ == '__main__':
-	planets = build_orbit_tree()
-	print(part_one())  # 308790
-	print(part_two())  # 472
+	main()
