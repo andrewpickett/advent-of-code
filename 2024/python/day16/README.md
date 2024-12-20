667924a06b1fc990ef6850aef4a3fe1b66072bcb
Andrew Pickett <picketta@gmail.com>
2024-12-19 21:48:12 -0500

    Day 16 write up start.

--- 2024/python/day16/README.md
+++ 2024/python/day16/README.md
@@ -1,20 +1,24 @@
 # Approach
 ### Data format
 
-
+Read the input as a `Grid`.
 
 ### Part 1
 > __
 
-
+Dijkstra. Just run it to get lowest cost to every node in the grid. Then just grab the value for the End node.
 
 ### Part 2
 > __
 
+Now that we have the full path with every node's least cost, we can backtrack from the end back to the start to determine
+which nodes are on a best path.
+
+TODO: Write a bit more detail here.
 
 # Results
 
 |              | Exec. Time (ms) |
 |--------------|----------------:|
-| **Part One** |                 |
-| **Part Two** |                 |
+| **Part One** |             157 |
+| **Part Two** |             306 |
--- 2024/python/day16/main.py
+++ 2024/python/day16/main.py
@@ -1,12 +1,51 @@
 from utils.timers import run_with_timer, get_data_with_timer
+from utils.grid import Grid
+from utils.utils import DIRS, DIR_COORDS, turn_around
+from collections import deque, defaultdict
 
 
 def get_data(filename):
-	return [x.strip() for x in open(filename).readlines()]
+	return Grid(file=filename)
+
+
+def dijkstra(grid, start):
+	q = deque([start])
+	came_from = {}
+	cost_so_far = defaultdict(tuple)
+	came_from[start] = None
+	cost_so_far[start[0]] = (0, DIRS[">"])
+
+	while q:
+		x = q.popleft()
+		for dir in DIR_COORDS:
+			next_node = grid.get_point(coords=tuple(sum(z) for z in zip(x[0].get_coord(), dir)))
+			if next_node and next_node.get_value() != "#" and dir != turn_around(x[1]):
+				new_cost = cost_so_far[x[0]][0] + (1 if dir == cost_so_far[x[0]][1] else 1001)
+				if next_node not in cost_so_far or new_cost < cost_so_far[next_node][0]:
+					cost_so_far[next_node] = (new_cost, dir)
+					q.append((next_node, dir, new_cost))
+					came_from[next_node] = x
+	return came_from, cost_so_far
+
+
+def reconstruct_path(came_from, start, end):
+	current = end
+	path = []
+	if end not in came_from:
+		return []
+	while current != start and current not in path:
+		path.append(current)
+		current = came_from[current][0]
+	path.append(start)
+	path.reverse()
+	return path
 
 
 def part_one(d):
-	return
+	start = [x for x in d.get_points() if x.get_value() == "S"][0]
+	end = [x for x in d.get_points() if x.get_value() == "E"][0]
+	path, cost = dijkstra(d, (start, DIRS[">"], 0), end)
+	return cost[end][0]
 
 
 def part_two(d):
