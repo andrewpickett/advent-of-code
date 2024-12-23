f0c93a62f69c9b4a8f9f392b99792cb265c4e2b7
Andrew Pickett <picketta@gmail.com>
2024-12-23 10:44:50 -0500

    Day 22.

--- 2024/python/day22/main.py
+++ 2024/python/day22/main.py
@@ -1,16 +1,46 @@
+from collections import defaultdict
+
 from utils.timers import run_with_timer, get_data_with_timer
 
 
 def get_data(filename):
-	return [x.strip() for x in open(filename).readlines()]
+	return [int(x.strip()) for x in open(filename).readlines()]
 
 
 def part_one(d):
-	return
+	secrets = defaultdict(int)
+	for x in d:
+		calc_secret(x, secrets)
+	return sum(x for x in secrets.values())
+
+
+def calc_secret(x, secrets):
+	last = x % 10
+	pattern = []
+	i = x
+	for _ in range(2000):
+		i = ((i*64) ^ i) % 16777216
+		i = ((i // 32) ^ i) % 16777216
+		i = ((i * 2048) ^ i) % 16777216
+		temp = i % 10
+		pattern.append((temp - last, temp))
+		last = temp
+	secrets[x] = i
+	return pattern
 
 
 def part_two(d):
-	return
+	secrets = defaultdict(int)
+	for x in d:
+		pattern = calc_secret(x, {})
+		seen = set()
+		for i in range(len(pattern)-4):
+			pat = tuple(x[0] for x in pattern[i:i+4])
+			val = pattern[i+3][1]
+			if pat not in seen:
+				seen.add(pat)
+				secrets[pat] += val
+	return max(secrets.values())
 
 
 def main(f="input.txt"):
