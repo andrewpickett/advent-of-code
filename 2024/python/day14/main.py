8f2f5393df4552617fb0a183fafb2604ff3e34a7
Andrew Pickett <picketta@gmail.com>
2025-01-17 15:23:42 -0500

    Clean up code.

--- 2024/python/day14/main.py
+++ 2024/python/day14/main.py
@@ -9,13 +9,16 @@ def get_data(filename):
 	return {"robots": [{"pos": tuple([int(y) for y in x[0][2:].split(",")]), "vel": tuple([int(y) for y in x[1][2:].split(",")])} for x in lines], "range": (103, 101)}
 
 
+def get_quads(grid):
+	quad1 = sum(grid[y][x] for y in range(0, len(grid) // 2) for x in range(0, len(grid[0]) // 2))
+	quad2 = sum(grid[y][x] for y in range((len(grid) // 2) + 1, len(grid)) for x in range(0, len(grid[0]) // 2))
+	quad3 = sum(grid[y][x] for y in range(0, len(grid) // 2) for x in range((len(grid[0]) // 2) + 1, len(grid[0])))
+	quad4 = sum(grid[y][x] for y in range((len(grid) // 2) + 1, len(grid)) for x in range((len(grid[0]) // 2) + 1, len(grid[0])))
+	return quad1, quad2, quad3, quad4
+
+
 def part_one(d):
-	grid = populate_grid_and_move(d, 100, 0, lambda x: x + 1)
-	quad1 = eval_quad(grid, range(0, len(grid) // 2), range(0, len(grid[0]) // 2))
-	quad2 = eval_quad(grid, range(0, len(grid) // 2), range((len(grid[0]) // 2) + 1, len(grid[0])))
-	quad3 = eval_quad(grid, range((len(grid) // 2) + 1, len(grid)), range(0, len(grid[0]) // 2))
-	quad4 = eval_quad(grid, range((len(grid) // 2) + 1, len(grid)), range((len(grid[0]) // 2) + 1, len(grid[0])))
-	return prod([quad1, quad2, quad3, quad4])
+	return prod(get_quads(populate_grid_and_move(d, 100, 0, lambda x: x + 1)))
 
 
 def part_two(d):
@@ -23,7 +26,7 @@ def part_two(d):
 	while True:
 		grid = populate_grid_and_move(d, 1, ".", lambda x: "#")
 		for row in grid:
-			if ''.join(row).find("#######") >= 0:
+			if ''.join(row).find("########") >= 0:
 				return i
 		i += 1
 
@@ -39,10 +42,6 @@ def populate_grid_and_move(d, time, fill_char, robot_func):
 def move(d, time, height, width):
 	for robot in d:
 		robot["pos"] = ((robot["pos"][0] + time*robot["vel"][0]) % width,(robot["pos"][1] + time*robot["vel"][1]) % height)
-
-
-def eval_quad(grid, yrange, xrange):
-	return sum(grid[y][x] for y in yrange for x in xrange)
 
 
 def main(f="input.txt"):
